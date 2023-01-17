062039de251a3bc955e8e660601f8fb0a482b142
Michael Peter <michaelpeter.ai@gmail.com>
2023-01-17 17:04:32 +0100

    feat: initial commit

---
+++ main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - a monty interpreter for working with stacks and queues
+ * data structures
+ * @argc: arguments count
+ * @argv: arguments vector
+ * Return: 0 (success), non-zero on error
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Welcome to Stacks and Queues\n");
+	printf("File to parse: %s\n", argv[1]);
+	printf("Work in progress...\n");
+
+	return (0);
+}
