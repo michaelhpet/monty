3bb8bd6693e1f7ed9a42549b47847d7c74c3ad19
Michael Peter <michaelpeter.ai@gmail.com>
2023-01-18 18:40:21 +0100

    feat: pall

---
+++ pall.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+
+/**
+ * pall - prints all the values on the stack, starting from the top of the stack
+ * @stack: double pointer to head node
+ * @line_number: line number of file where instruction was parsed
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
