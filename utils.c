9e468368fbf3dd50ebbedc30519657662547adb0
Michael Peter <michaelhpet@gmail.com>
2023-03-22 18:33:07 +0100

    refactor: free_stack in utils

--- utils.c
+++ utils.c
@@ -22,3 +22,25 @@ int nan(char *s)
 
 	return (0);
 }
+
+/**
+ * free_stack - frees the stack
+ * @stack: pointer to head node
+ */
+void free_stack(stack_t *stack)
+{
+	stack_t *current = stack;
+
+	if (current == NULL)
+		return;
+
+	while (current->next)
+	{
+		current = current->next;
+		free(current->prev);
+	}
+
+	if (current)
+		free(current);
+}
+
